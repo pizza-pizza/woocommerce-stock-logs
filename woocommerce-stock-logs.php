54ac562fa370d0cd77ff6f939e502bdbe5d94113
benrawn <ben.rawn@gmail.com>
2015-01-30 14:56:08 +0100

    i18n, new change notes, adding acf label configuration support

--- woocommerce-stock-logs.php
+++ woocommerce-stock-logs.php
@@ -21,9 +21,15 @@ class wcStockLogs {
 	public function __construct() {
 		global $pagenow;
 		define( 'WC_STOCKLOGS_TABLE', 'woocommerce_stock_adjustments' );
+
+		load_plugin_textdomain( 'woocommerce-stock-logs', false, basename( dirname(__FILE__) ) . '/i18n' );
+
 		add_action( 'init', array( $this, 'stocklogs_init_db' ) );
 		add_action( 'wp_ajax_save_wcstock_adjustment', array( $this, 'save_stock_adjustment' ) );
 		add_action( 'wp_ajax_load_wcstock_report', array( $this, 'render_report_meta_box' ) );
+
+		add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), array( $this, 'plugin_action_links' ) );
+		add_filter( 'woocommerce_inventory_settings', array( $this, 'render_plugin_settings' ) );
 
 		if ( in_array( $pagenow, array( 'post.php' ) ) ) add_action( 'add_meta_boxes', array( $this, 'add_metaboxes' ) );
 	}
@@ -36,7 +42,7 @@ class wcStockLogs {
 
 			add_meta_box(
 				'stock_adjustment_inputs'
-				,'Adjust Stock Quantity'
+				,__( 'Adjust Stock Quantity', 'woocommerce-stock-logs' )
 				,array( $this, 'render_adjustment_meta_box' )
 				,$post_type
 				,'side'
@@ -45,7 +51,7 @@ class wcStockLogs {
 
 			add_meta_box(
 				'stock_adjustment_log'
-				,'Product Stock History'
+				,__( 'Product Stock History', 'woocommerce-stock-logs' )
 				,array( $this, 'render_report_meta_box' )
 				,$post_type
 				,'advanced'
@@ -67,14 +73,21 @@ class wcStockLogs {
 		$ajax_data = "{post_ID:{$post->ID},adjustment:jQuery('#product_stock_adjustment').val(),notes:jQuery('#product_stock_adjustment_notes').val()}";
 		$ajax_submit = "{$ajax_before}jQuery.post(ajaxurl + '?action=save_wcstock_adjustment',{$ajax_data},{$ajax_success});return false;";
 
-		$adjustment_notes = array( '' => 'Select an action','New Arrival','Trash','Correction' );
+		$adjustment_notes = array(
+			'' => __( 'Select an action', 'woocommerce-stock-logs' ),
+			__( 'New arrival', 'woocommerce-stock-logs' ),
+			__( 'Damaged goods', 'woocommerce-stock-logs' ),
+			__( 'Delivery incomplete', 'woocommerce-stock-logs' ),
+			__( 'Stock movement', 'woocommerce-stock-logs' ),
+			__( 'Other…', 'woocommerce-stock-logs' ) );
+		$unit_label_field = get_option( 'wc_stocklogs_acf_label', 1 );
+		$unit_label = $unit_label_field ? get_field( $unit_label_field, $post->ID ) : NULL;
 
-		echo '<label for="product_stock_adjustment" style="display:inline-block;margin-bottom:5px;">Adjust Stock Quantity:</label>';
-		echo '<input type="number" id="product_stock_adjustment" style="width:100%;" />';
-		echo '<select id="product_stock_adjustment_notes" style="width:100%;">';
+		echo '<input type="number" id="product_stock_adjustment" placeholder="0" style="width:' . ( $unit_label ? 70 : 100 ) . '%;" />' . ( $unit_label ? "<span style='display:inline-block;width:29%;text-align:center;'> x $unit_label</span>" : '' );
+		echo '<select id="product_stock_adjustment_notes" style="width:100%;margin-top:5px;">';
 		foreach ( $adjustment_notes as $k => $n ) echo '<option value="' . (!is_numeric($k) ? $k : $n) . '">' . $n . '</option>';
 		echo '</select>';
-		echo '<a class="button button-primary button-large" href="#" onclick="' . $ajax_submit . '" style="margin-top:5px;">Save Adjustment</a> ';
+		echo '<a class="button button-primary button-large" href="#" onclick="' . $ajax_submit . '" style="margin-top:5px;">' . __( 'Save Adjustment', 'woocommerce-stock-logs' ) . '</a> ';
 		echo '&nbsp;<img src="images/loading.gif" style="display:none;padding-top:12px;" />';
 	}
 
@@ -91,7 +104,7 @@ class wcStockLogs {
 
 		echo '<style type="text/css">#stock_adjustment_log th { border-bottom:1px solid #eee;padding-bottom:2px; } #stock_adjustment_log td { padding-top:5px; }</style>';
 		echo '<table cellpadding="0" cellspacing="0" width="100%">';
-		echo '<thead><tr><th align="left">Date / Time</th><th align="left">User</th><th align="right">Initial Quantity</th><th align="right">Adjustment</th><th align="right" style="padding-right:20px;">Adjusted Quantity</th><th align="left">Note</th></thead><tbody>';
+		echo '<thead><tr><th align="left">' . __( 'Date / Time', 'woocommerce-stock-logs' ) . '</th><th align="left">' . __( 'User', 'woocommerce-stock-logs' ) . '</th><th align="right">' . __( 'Initial Quantity', 'woocommerce-stock-logs' ) . '</th><th align="right">' . __( 'Adjustment', 'woocommerce-stock-logs' ) . '</th><th align="right" style="padding-right:20px;">' . __( 'Adjusted Quantity', 'woocommerce-stock-logs' ) . '</th><th align="left">' . __( 'Note', 'woocommerce-stock-logs' ) . '</th></thead><tbody>';
 		foreach ( $adjustments as $a ) {
 			$user = get_user_by( 'id', $a->user_ID );
 			$initial_quantity = $a->adjusted_quantity - $a->adjustment;
@@ -99,7 +112,49 @@ class wcStockLogs {
 		}
 		echo '</tbody></table>';
 
-		if(defined('DOING_AJAX') && DOING_AJAX) die();
+		if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) die();
+	}
+
+	/**
+	 * Let's add the Settings link on the plugin page
+	 */
+	public function plugin_action_links( $links ) {
+		$links[] = '<a href="admin.php?page=wc-settings&tab=products&section=inventory">Settings</a>';
+	   return $links;
+	}
+
+	/**
+	 * Let's update the Settings page
+	 */
+	public function render_plugin_settings( $settings ) {
+		$select_fields = array('' => '');
+		$acf_fields = get_posts( array( 'post_type' => 'acf-field' ) );
+		foreach ( $acf_fields as $field ) $select_fields[$field->post_excerpt] = $field->post_title;
+
+		$updated_settings = array();
+		foreach ( $settings as $section ) {
+			$updated_settings[] = $section;
+
+			if ( isset( $section['id'] ) && 'inventory_options' == $section['id'] && isset( $section['type'] ) && 'sectionend' == $section['type'] ) {
+				$updated_settings[] = array(
+					'name' => __( 'Stock Log Options', 'woocommerce-stock-logs' ),
+					'type' => 'title',
+					'id'   => 'wc_stocklogs_global_settings',
+				);
+				$updated_settings[] = array(
+					'title'		=> __( 'Unit / Quantity Label', 'woocommerce-stock-logs' ),
+					'desc'		=> __( 'Optionally specify an ACF field to designate products\' sale unit (e.g. "0.5kg")', 'woocommerce-stock-logs' ),
+					'id'		=> 'wc_stocklogs_acf_label',
+					'default'	=> '',
+					'type'		=> 'select',
+					'options'	=> $select_fields,
+					'desc_tip'	=>  true
+				);
+				$updated_settings[] = array( 'type' => 'sectionend', 'id' => 'wc_stocklogs_settings' );
+			}
+		}
+
+		return $updated_settings;
 	}
 
 	/**
@@ -123,7 +178,7 @@ class wcStockLogs {
 	/**
 	 * Initialize our log table.
 	 */
-	public function stocklogs_init_db() {
+	function stocklogs_init_db() {
 		global $wpdb;
 
 		if ( in_array( $wpdb->prefix . WC_STOCKLOGS_TABLE, $wpdb->get_col( 'show tables' ) ) === false ){
